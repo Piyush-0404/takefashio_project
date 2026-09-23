a2f03bb5e3b7365775b485b9c63431e82e774ce1
varundubey2804 <varundubey2804@gmail.com>
2026-09-24 02:36:06 +0530

    Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -15,11 +15,11 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PATCH,DELETE,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Accept" },
-          { key: "Access-Control-Allow-Origin", value: "https://takefashion-frontend.vercel.app" },
+          // { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Methods", value: "GET,POST,PATCH,DELETE,OPTIONS" },
+          // { key: "Access-Control-Allow-Headers", value: "Content-Type, Accept" },
+          // { key: "Access-Control-Allow-Origin", value: "https://takefashion-frontend.vercel.app" },
         ],
       },
     ];
