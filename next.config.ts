5d7ba008e8aa2166fed4902c7de530dada95ebce
varundubey2804 <varundubey2804@gmail.com>
2026-09-24 02:43:41 +0530

    Fix Next.js page extensions

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,12 @@ import path from "node:path";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  pageExtensions: ["ts", "tsx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   turbopack: {
     root: path.resolve(__dirname),
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          // { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
-          // { key: "Access-Control-Allow-Credentials", value: "true" },
-          // { key: "Access-Control-Allow-Methods", value: "GET,POST,PATCH,DELETE,OPTIONS" },
-          // { key: "Access-Control-Allow-Headers", value: "Content-Type, Accept" },
-          // { key: "Access-Control-Allow-Origin", value: "https://takefashion-frontend.vercel.app" },
-        ],
-      },
-    ];
   },
 };
 
